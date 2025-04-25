aed3eade9eed72643ab072c3158e3eeb5e116487
hassan1233-moja <mahmodulhassanmojahid19@gmail.com>
2025-04-25 09:30:45 +0600

    updat code for embad exust

--- index.js
+++ index.js
@@ -171,8 +171,14 @@ function loadCourseList() {
 function showVideo(videoUrl) {
   const videoContainer = document.getElementById('video-box');
   videoContainer.innerHTML = `
-    <iframe width="560" height="315" src="${videoUrl}" 
-    frameborder="0" allowfullscreen></iframe>
+    <div style="position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%;">
+      <iframe 
+        src="${videoUrl}" 
+        frameborder="0" 
+        allowfullscreen
+        style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;">
+      </iframe>
+    </div>
   `;
 }
 
